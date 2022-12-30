e04cd03ab7a61a1b0fa998df4e0e83a36a5d864d
skimmfish <seoxints@gmail.com>
2022-12-30 20:45:12 +0100

    update

--- resources/views/layouts/footer_one.blade.php
+++ resources/views/layouts/footer_one.blade.php
@@ -103,7 +103,7 @@
 </svg>
 -->
 
-<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="1400.000000pt" height="1400.000000pt" viewBox="0 0 600.000000 600.000000"
+<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="1400.000000pt" height="1400.000000pt" viewBox="0 0 300.000000 300.000000"
  preserveAspectRatio="xMidYMid meet" class="logo-offerta-footer site-logo">
 
 <g transform="translate(0.000000,500.000000) scale(0.100000,-0.100000)"
