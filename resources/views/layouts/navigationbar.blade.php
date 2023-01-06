9f66640c32cfe2f6ded4e018ff77f26cea44f8e5
skimmfish <seoxints@gmail.com>
2023-01-06 16:56:25 +0100

    update

---
+++ resources/views/layouts/navigationbar.blade.php
@@ -0,0 +1,118 @@
+<div class="new-header__menu" role="navigation">
+
+    <a href="{{route('anslut-ditt-foretag')}}" class="new-header__menu--item">Anslut företag</a>  
+          
+            <a href="{{route('login')}}" class="new-header__menu--item">Logga in</a>
+        
+    <a class="nav-icon" data-toggle-menu="mobile-nav">
+        <div class="text-close">Stäng</div>
+        <div class="text-menu">M</div>
+        </a>
+
+    <input type="checkbox" id="active" style="background:none">
+    <label for="active" class="menu-btn" onclick="myFunction(this)" data-toggle="collapse" data-target="#navbarCollapse">
+    <i class="fa fa-bars"></i>
+  </label>
+
+<div class="wrapper">
+      <ul>
+      <li><a href="{{route('kontactos-pg')}}" class="top-section__heading" style="font-family:Open Sans">Kontakta Oss</a></li>
+      <li><a href="{{route('yrkeskategorier')}}">Yrkeskategorier</a></li>
+      <li><a href="{{route('pris')}}">Pris</a></li>
+      <hr/>
+<li><span class="text-sm">{{config('app.name')}} Group AB</span><span><a href="tel:010-33 020 11" class="text-sm">010-33 020 11</a></span>
+<span><a href="mailto:info@toppoffert.se" class="text-sm">info@toppoffert.se</a></span>
+<span>
+
+                            <a href="https://www.facebook.com/toppoffert.se/" class="social-icons">
+                                
+                
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon-small">
+                  <path d="M18.14 7.17a.5.5 0 0 0-.37-.17H14V5.59c0-.28.06-.6.51-.6h3a.44.44 0 0 0 .35-.15.5.5 0 0 0 .14-.34v-4a.5.5 0 0 0-.5-.5h-4.33C8.37 0 8 4.1 8 5.35V7H5.5a.5.5 0 0 0-.5.5v4a.5.5 0 0 0 .5.5H8v11.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 .5-.5V12h3.35a.5.5 0 0 0 .5-.45l.42-4a.5.5 0 0 0-.13-.38z" />
+                </svg>
+                
+                            </a>
+                            <a href="https://www.instagram.com/toppoffert.se/" class="social-icons">
+                                
+                
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon-small">
+                  <path d="M17.5 0h-11A6.51 6.51 0 0 0 0 6.5v11A6.51 6.51 0 0 0 6.5 24h11a6.51 6.51 0 0 0 6.5-6.5v-11A6.51 6.51 0 0 0 17.5 0zM12 17.5a5.5 5.5 0 1 1 5.5-5.5 5.5 5.5 0 0 1-5.5 5.5zm6.5-11A1.5 1.5 0 1 1 20 5a1.5 1.5 0 0 1-1.5 1.5z" />
+                </svg>
+                
+                            </a>
+                
+
+</span>
+</li>
+</ul>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+</div>
+
+<nav class="new-main-menu">
+    <div class="new-main-menu__inner">
+        <div class="new-main-menu__menu">
+            <a href="{{route('anslut-ditt-foretag')}}" class="new-main-menu__menu--item mobile">Anslut företag</a>
+            <a href="{{route('login')}}" class="new-main-menu__menu--item mobile">Logga in</a>
+            <a href="{{route('kontactos-pg')}}" class="new-main-menu__menu--item">Kontakta oss</a>
+           </div>
+        </div>
+    
+        <ul class="new-main-menu__footer">
+        <li class="new-main-menu__footer--item">
+            {{config('app.name')}} Group AB
+        </li>
+        <li class="new-main-menu__footer--item">
+            <a href="tel:010-33 020 11">010-33 020 11</a>
+        </li>
+        <li class="new-main-menu__footer--item">
+            <a href="mailto:info@offerta.se">info@toppoffert.se</a>
+        </li>
+        <li class="new-main-menu__footer--item">
+            <a href="https://www.linkedin.com/company/" class="social-icons">
+                
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon-small">
+  <rect x="2" y="8.5" width="5" height="14" rx=".5" ry=".5" />
+  <ellipse cx="4.48" cy="4" rx="2.48" ry="2.5" />
+  <path d="M18.5 22.5h3a.5.5 0 0 0 .5-.5v-8.4C22 9.83 19.87 8 16.89 8a4.21 4.21 0 0 0-3.17 1.27A.41.41 0 0 1 13 9a.5.5 0 0 0-.5-.5h-3A.5.5 0 0 0 9 9v13a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-7.5a2.5 2.5 0 0 1 5 0V22a.5.5 0 0 0 .5.5z" />
+</svg>
+
+            </a>
+            <a href="https://www.facebook.com/toppoffert.se/" class="social-icons">
+                
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon-small">
+  <path d="M18.14 7.17a.5.5 0 0 0-.37-.17H14V5.59c0-.28.06-.6.51-.6h3a.44.44 0 0 0 .35-.15.5.5 0 0 0 .14-.34v-4a.5.5 0 0 0-.5-.5h-4.33C8.37 0 8 4.1 8 5.35V7H5.5a.5.5 0 0 0-.5.5v4a.5.5 0 0 0 .5.5H8v11.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 .5-.5V12h3.35a.5.5 0 0 0 .5-.45l.42-4a.5.5 0 0 0-.13-.38z" />
+</svg>
+
+            </a>
+            <a href="https://www.instagram.com/toppoffert.se/" class="social-icons">
+                
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon-small">
+  <path d="M17.5 0h-11A6.51 6.51 0 0 0 0 6.5v11A6.51 6.51 0 0 0 6.5 24h11a6.51 6.51 0 0 0 6.5-6.5v-11A6.51 6.51 0 0 0 17.5 0zM12 17.5a5.5 5.5 0 1 1 5.5-5.5 5.5 5.5 0 0 1-5.5 5.5zm6.5-11A1.5 1.5 0 1 1 20 5a1.5 1.5 0 0 1-1.5 1.5z" />
+</svg>
+
+            </a>
+        </li>
+    </ul>
+</nav>
+          
+    <div class="header-material-circle"></div>
+      </div>
